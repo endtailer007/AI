a69c22e4001e2060f154185e097cab82174680c0
endtailer <surajofficial2000@gmail.com>
2021-11-06 12:45:00 +0530

    COMMIT1

---
+++ intro to python.py
@@ -0,0 +1,23 @@
+import math as m
+x=2.5
+y=m.trunc(x)
+print(x)
+print(y)
+print("Hello world")
+#data types in python
+print("String, int, float, lists, dictionaries,sets,bool are the basic data types in python language.")
+x="    This is python tutorial.      "
+print(x.rjust(25))
+print(x.ljust(25))
+print(x.center(25))#These are used to align text to right or left
+print(x)
+print(x.lstrip())#strip functions remove extra whitespaces
+print(x.rstrip())
+print(x.strip())
+z='123'
+print(z.isnumeric())
+print(x.replace("py","sai"))
+print(x.split('i'))#This will split the string at a given character
+
+
+
